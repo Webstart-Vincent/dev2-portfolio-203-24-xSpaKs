1d2091d172683d5d82de8ba6f7f393cf6e7989f9
Aran Hiblot <a.hiblot@student.isartdigital.com>
2024-02-07 14:07:07 +0100

    Fixed typo

--- components/Frame.js
+++ components/Frame.js
@@ -11,10 +11,10 @@ const Frame = ({ href, src, alt, width }) => {
             <Image src={src} alt={alt} />
 
             <div className="p-1 absolute top-0 text-secondaryColor w-[100%] group-hover/projet:text-detailsColor duration-300 transition-colors flex bg-black/60 h-[30px] justify-between gap-5">
-                <p className="font-mono text-sm whitespace-nowrap">
+                <p className="font-mono text-base whitespace-nowrap">
                     24/01/2023
                 </p>
-                <p className="font-mono text-sm whitespace-nowrap truncate">
+                <p className="font-mono text-base whitespace-nowrap truncate">
                     Titre du projet
                 </p>
             </div>
