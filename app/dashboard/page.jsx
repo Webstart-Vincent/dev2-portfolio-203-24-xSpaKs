7c507a3bbca68954b45d5f433172d90aad042909
Aran Hiblot <a.hiblot@student.isartdigital.com>
2024-02-28 16:12:46 +0100

    Redirect if not logged in

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,14 @@
 "use client";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
     const { data: session, status } = useSession();
+    const router = useRouter();
+    if (!session) {
+        router.push("/");
+    }
+
     return (
         <>
             <h1>Tableau de bord</h1>
